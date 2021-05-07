41ffd0a4a79643694e236edd2f91bee4b16f4651
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2021-05-07 16:05:54 +0200

    added RollDice method + new component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Vue dobbelstenen"/>
+    <button @click="RollDice">Rol dobbelstenen</button>
   </div>
 </template>
 
 <script>
+//import DisplayResult from './components/DisplayResult.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    //DisplayResult,
+  },
+
+  methods: {
+    RollDice() {  
+      //let rolledNumbers = [0, 0, 0, 0, 0, 0];
+
+      for (let i = 0; i < this.dices.length; i++) {
+        this.dices[i] = Math.floor(Math.random() * 6) + 1;
+      }
+      this.dices = this.dices.sort();
+    }
+  },
+
+    data() {
+      return {
+        dices: [0, 0, 0, 0, 0, 0]
+    }
+  },
 }
+
 </script>
 
 <style>
